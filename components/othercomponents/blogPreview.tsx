bfe1095f7e7451b468c4f04119f47b7e0c40d89d
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2025-04-28 11:56:08 +0200

    Enhance BlogPreview component styling for improved visibility and scrollbar handling

--- components/othercomponents/blogPreview.tsx
+++ components/othercomponents/blogPreview.tsx
@@ -7,7 +7,7 @@ interface BlogPreviewProps {
 function BlogPreview({ content }: BlogPreviewProps) {
   return (
     <div
-      className="prose max-h-[80vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-transparent"
+      className="prose prose-invert max-h-[100vh] overflow-y-auto scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-transparent text-white"
       dangerouslySetInnerHTML={{
         __html: content,
       }}
